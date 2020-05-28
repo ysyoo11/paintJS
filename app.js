ceb077992a3a9487ffe78da959cdbe60baec708e
ysyoo11 <62553092+ysyoo11@users.noreply.github.com>
2020-05-28 17:51:38 +0900

    brush size (range)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector(".js-canvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("js-color");
+const range = document.querySelector(".js-range");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -35,6 +36,11 @@ function handleColorClick(event) {
   ctx.strokeStyle = color;
 }
 
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size * 2;
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -45,3 +51,7 @@ if (canvas) {
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleColorClick)
 );
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
